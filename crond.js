9460b7ff08ea55e3ba9f9d2f996be51acddf77cf
xialeistudio <xialeistudio@gmail.com>
2016-05-19 11:07:56 +0800

    统计完成

--- crond.js
+++ crond.js
@@ -12,6 +12,7 @@ var Promise = require('bluebird');
 var fs = require('fs');
 var config = require('./config.json');
 var _ = require('underscore');
+var URL = require('url');
 Promise.promisifyAll(fs);
 /**
  * 初始化目录
@@ -32,6 +33,30 @@ function initPath(path) {
 		return path;
 	});
 }
+var dbData = [];//最终入库数据
+//url,requestCount,totalTime,averageTime,minTime,maxTime,date
+function mergeDbData(mysqlData) {
+	if (dbData.length === 0) {
+		dbData = dbData.concat(mysqlData);
+	}
+	else {
+		dbData = dbData.map(function(dbItem) {
+			var isMatched = false;
+			mysqlData.forEach(function(mysqlItem) {
+				if (mysqlItem.url == dbItem.url) {
+					isMatched = true;
+					//进行数据合并计算
+					dbItem.requestCount += mysqlItem.requestCount;
+					dbItem.totalTime += mysqlItem.totalTime;
+					dbItem.averageTime = parseFloat((dbItem.totalTime + mysqlItem.totalTime) / dbItem.requestCount).toFixed(3);
+					dbItem.minTime = Math.min(dbItem.minTime, mysqlItem.minTime);
+					dbItem.maxTime = Math.max(dbItem.maxTime, mysqlItem.maxTime);
+				}
+			});
+			return dbItem;
+		});
+	}
+}
 /**
  * 解压出来的数据转换为Mysql数组
  * @param data
@@ -49,10 +74,11 @@ function uncompressDataToMysqlData(data, date) {
 			var json = JSON.parse(line);
 			var url = json.url;
 			var rtime = parseFloat(json.rtime);
+			var status = json.status;
 			//检测是否存在
 			var isMatched = false, matchIndex = -1;
 			needData.forEach(function(item, index) {
-				if (item.url === url) {
+				if (getUrlPattern(item.url) === getUrlPattern(url)) {
 					isMatched = true;
 					matchIndex = index;
 				}
@@ -60,13 +86,14 @@ function uncompressDataToMysqlData(data, date) {
 			var record;
 			if (!isMatched) {
 				record = {
-					url: url,
+					url: getUrlPattern(url),
 					requestCount: 1,
 					totalTime: rtime,
 					averageTime: rtime,
 					minTime: rtime,
 					maxTime: rtime,
-					date: date
+					date: date,
+					status: status
 				};
 				needData.push(record);
 			}
@@ -87,7 +114,7 @@ function uncompressDataToMysqlData(data, date) {
 	//mysql数据处理
 	var mysqlData = [];
 	needData.forEach(function(item) {
-		var row = [item.url, item.requestCount, item.totalTime, item.averageTime, item.minTime, item.maxTime, item.date];
+		var row = [getUrlPattern(item.url), item.requestCount, item.totalTime, item.averageTime, item.minTime, item.maxTime, item.date, item.status];
 		mysqlData.push(row);
 	});
 	return mysqlData;
@@ -98,7 +125,7 @@ function uncompressDataToMysqlData(data, date) {
  * @returns {*}
  */
 function saveToDb(mysqlData) {
-	var sql = 'INSERT INTO reg_logs (url,requestCount,totalTime,averageTime,minTime,maxTime,date) VALUES ?';
+	var sql = 'INSERT INTO ' + config.mysql.tablePrefix + 'logs (url,requestCount,totalTime,averageTime,minTime,maxTime,date,status) VALUES ?';
 	//MySQL批量插入
 	return database().then(function(conn) {
 		return conn.queryAsync(sql, [mysqlData]).then(function(res) {
@@ -107,6 +134,41 @@ function saveToDb(mysqlData) {
 		}).catch(function(e) {
 			conn.release();
 			throw e;
+		});
+	});
+}
+/**
+ * 获取URL模式
+ * @param url
+ * @returns {string|null|string|*}
+ */
+function getUrlPattern(url) {
+	url = URL.parse(url);
+	var link = url.pathname;
+	if (url.query !== null) {
+		var params = url.query.split('&');
+		params = params.map(function(item) {
+			var temp = item.split('=');
+			return temp[0] + '=';
+		});
+		link += '?' + params.join('&');
+	}
+	return link;
+}
+/**
+ * 删除目录
+ * @param path
+ */
+function unlinkPath(path) {
+	var exec = require('child_process').exec, child;
+	return new Promise(function(resolve, reject) {
+		child = exec('rm -rf ' + path, function(err, out) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(out);
+			}
 		});
 	});
 }
@@ -135,23 +197,49 @@ function run() {
 			.then(function() {
 				//下载
 				var ee = oss.download(snappyPath, {
-					'max-keys': 1000,
+					'max-keys': 5,
 					'prefix': keyPrefix
 				});
 				var decoded = 0;
+				logger.console.info('procesing');
 				ee.on('object', function(name, localPath, downloaded, total) {
+					if (name === null) {
+						decoded++;
+						logger.console.trace('[mysql] ' + decoded + '/' + total + ' - ' + parseInt(decoded * 100 / total) + '%');
+						if (decoded >= total) {
+							process.exit(0);
+						}
+						return;
+					}
 					logger.console.trace('[oss] ' + downloaded + '/' + total + ' - ' + parseInt(downloaded * 100 / total) + '%');
 					//开始解压
 					snappy.decodeFile(localPath).then(function(data) {
 						var mysqlData = uncompressDataToMysqlData(data, yesterdayStr);
-						return saveToDb(mysqlData).then(function(res) {
-							logger.console.info('[mysql] info: %s', JSON.stringify(res));
-						});
+						//合并处理
+						return mergeDbData(mysqlData);
 					}).catch(function(e) {
 						logger.console.error('[snappy] parse %s error: %s', localPath, e.message);
 					}).then(function() {
 						decoded++;
-						logger.console.trace('[mysql] ' + decoded + '/' + total + ' - ' + parseInt(decoded * 100 / total) + '%');
+						logger.console.info('[mysql] ' + decoded + '/' + total + ' - ' + parseInt(decoded * 100 / total) + '%');
+						if (decoded >= total) {
+							//全部处理完毕，入库
+							return saveToDb(dbData).then(function() {
+								return 'ok';
+							}).catch(function() {
+								return 'error';
+							}).then(function(result) {
+								logger.console.info('[mysql] save: ' + result);
+								return unlinkPath(snappyPath)
+										.then(function() {
+											logger.console.info('[clean] ' + snappyPath + ' success');
+											process.exit(0);
+										}).catch(function(e) {
+											logger.console.error('[clean] ' + snappyPath + ' error: ' + e.message);
+											process.exit(0);
+										});
+							});
+						}
 					});
 				});
 				ee.on('end', function() {
